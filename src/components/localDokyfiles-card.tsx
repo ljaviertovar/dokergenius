687d412118834df9e6f141227c8f12d30256efc8
ljaviertovar <luisjavier_tovar@outlook.com>
2023-09-01 14:51:47 -0400

    update local dokyfiles

--- src/components/localDokyfiles-card.tsx
+++ src/components/localDokyfiles-card.tsx
@@ -2,11 +2,11 @@
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 
-import { useDokyfileStore } from '@/store/useDokyfileStore'
+// import { useDokyfileStore } from '@/store/useDokyfileStore'
 
 export default function LocalDokyfiles() {
 
-  const { dokyfiles, setCurrentDokyfile } = useDokyfileStore(state => state)
+  // const { dokyfiles, setCurrentDokyfile } = useDokyfileStore(state => state)
 
   return (
     <Card>
@@ -16,7 +16,7 @@ export default function LocalDokyfiles() {
       </CardHeader>
       <CardContent>
         <div className='grid gap-2 mh-44 overflow-auto'>
-          {dokyfiles.map(dokyfile => (
+          {/* {dokyfiles.map(dokyfile => (
             <div
               key={dokyfile.id}
               className='flex items-start w-full border space-x-4 rounded-md p-2 transition-all bg-[#020611] hover:bg-accent hover:text-accent-foreground cursor-pointer'
@@ -26,7 +26,7 @@ export default function LocalDokyfiles() {
             >
               <p className='text-sm text-muted-foreground'>{dokyfile.prompt}</p>
             </div>
-          ))}
+          ))} */}
         </div>
       </CardContent>
     </Card>
