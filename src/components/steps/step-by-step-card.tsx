d077cacf17c4340d747607cb0f86b3eb9532a061
ljaviertovar <luisjavier_tovar@outlook.com>
2023-09-03 13:56:36 -0400

    update step 1

--- src/components/steps/step-by-step-card.tsx
+++ src/components/steps/step-by-step-card.tsx
@@ -1,4 +1,9 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { ArrowRightIcon, ArrowLeftIcon } from '@radix-ui/react-icons'
+
+import BaseImageSelection from './step1/base-image-selection'
+
 import { useCreateStore } from '@/store/useCreateStore'
 
 export default function StepByStepCard() {
@@ -7,22 +12,52 @@ export default function StepByStepCard() {
   const Step = () => {
     switch (step) {
       case 1:
-        return <div>Step1</div>
+        return (
+          <BaseImageSelection
+            step={step}
+            setStep={setStep}
+          />
+        )
+      case 2:
+        return <div>Step2</div>
+      case 3:
+        return <div>Step3</div>
+      case 4:
+        return <div>Step4</div>
 
       default:
-        return <div></div>
+        return <div>1</div>
     }
   }
 
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Enter the prompt for your Dockerfile</CardTitle>
-        <CardDescription>You can enter instructions to generate or enter a Dockerfile to validate it.</CardDescription>
+        <CardTitle>Create a Dockerfile step-by-step</CardTitle>
+        <CardDescription>Follow the steps to create your personalize Dockerfile.</CardDescription>
       </CardHeader>
       <CardContent>
         <form>
           <Step />
+
+          <div className='flex flex-row gap-6'>
+            <Button
+              className='mt-6 w-full'
+              size={'sm'}
+              disabled={step <= 1}
+            >
+              <span className='mr-1'>
+                <ArrowLeftIcon className='mt-px h-5 w-5' />
+              </span>
+              Back
+            </Button>
+            <Button className='mt-6 w-full' size={'sm'}>
+              Next
+              <span className='ml-1'>
+                <ArrowRightIcon className='mt-px h-5 w-5' />
+              </span>
+            </Button>
+          </div>
         </form>
       </CardContent>
     </Card>
--- src/components/steps/step1/base-image-selection.tsx
+++ src/components/steps/step1/base-image-selection.tsx
@@ -1,58 +1,62 @@
-import { useState } from "react"
-import ImageOption from "./image-option"
+import { useState } from 'react'
+import ImageOption from './image-option'
 
-const BaseImageSelection = ({ onNext }) => {
-	// Lista de opciones de imagen
-	const [imageOptions] = useState([
-		{
-			name: "node:14",
-			description: "La imagen oficial de Node.js versión 14.",
-			features: ["Node.js 14", "Alpine Linux", "Tamaño: 69MB"],
-		},
-		{
-			name: "node:16",
-			description: "La imagen oficial de Node.js versión 16.",
-			features: ["Node.js 16", "Alpine Linux", "Tamaño: 72MB"],
-		},
-		{
-			name: "node:latest",
-			description: "La última versión de la imagen oficial de Node.js.",
-			features: ["Última versión de Node.js", "Alpine Linux", "Tamaño: 75MB"],
-		},
-	])
+interface Props {
+  step: number
+  setStep: (value: number) => void
+}
 
-	// Estado de la opción de imagen seleccionada
-	const [selectedImage, setSelectedImage] = useState(null)
+const BaseImageSelection = ({ step, setStep }: Props) => {
+  const [imageOptions] = useState([
+    {
+      name: 'node:14',
+      description: 'La imagen oficial de Node.js versión 14.',
+      features: ['Node.js 14', 'Alpine Linux', 'Tamaño: 69MB'],
+    },
+    {
+      name: 'node:16',
+      description: 'La imagen oficial de Node.js versión 16.',
+      features: ['Node.js 16', 'Alpine Linux', 'Tamaño: 72MB'],
+    },
+    {
+      name: 'node:latest',
+      description: 'La última versión de la imagen oficial de Node.js.',
+      features: ['Última versión de Node.js', 'Alpine Linux', 'Tamaño: 75MB'],
+    },
+  ])
 
-	// Función para manejar la selección de una opción de imagen
-	const handleImageSelection = image => {
-		setSelectedImage(image)
-	}
+  // Estado de la opción de imagen seleccionada
+  const [selectedImage, setSelectedImage] = useState(null)
 
-	return (
-		<div className=' rounded-md shadow p-4'>
-			<h2 className='text-lg font-medium mb-4'>Paso 1: Seleccionar la base de la imagen</h2>
-			<div className='flex flex-col space-y-4'>
-				{/* Lista de opciones de imagen */}
-				{imageOptions.map(image => (
-					<ImageOption
-						key={image.name}
-						image={image}
-						selected={selectedImage?.name === image.name}
-						onSelect={() => handleImageSelection(image)}
-					/>
-				))}
-			</div>
-			{/* Botón Siguiente */}
-			<button
-				className='bg-blue-500 text-white py-2 px-4 rounded mt-4 disabled:bg-gray-400 disabled:pointer-events-none'
-				disabled={!selectedImage}
-				onClick={onNext}
-			>
-				Siguiente
-			</button>
-		</div>
-	)
+  // Función para manejar la selección de una opción de imagen
+  const handleImageSelection = image => {
+    setSelectedImage(image)
+  }
+
+  return (
+    <div className=' rounded-md shadow p-4'>
+      <h2 className='text-lg font-medium mb-4'>Paso 1: Seleccionar la base de la imagen</h2>
+      <div className='flex flex-col space-y-4'>
+        {/* Lista de opciones de imagen */}
+        {imageOptions.map(image => (
+          <ImageOption
+            key={image.name}
+            image={image}
+            selected={selectedImage?.name === image.name}
+            onSelect={() => handleImageSelection(image)}
+          />
+        ))}
+      </div>
+      {/* Botón Siguiente */}
+      <button
+        className='bg-blue-500 text-white py-2 px-4 rounded mt-4 disabled:bg-gray-400 disabled:pointer-events-none'
+        disabled={!selectedImage}
+        onClick={() => setStep(step + 1)}
+      >
+        Next
+      </button>
+    </div>
+  )
 }
 
 export default BaseImageSelection
